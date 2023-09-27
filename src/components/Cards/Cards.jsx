3b562243b747b8bb5eead43ee0a82f75a91ba90d
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-09-27 15:27:08 +0600

    display all cards white the specific color

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,13 +1,20 @@
 
 import { useEffect, useState } from "react";
 import Card from "../Card/Card";
-const Cards = () => {
+import { PropTypes } from 'prop-types';
+const Cards = ({searchValue}) => {
+   const value = searchValue;
+   console.log(value);
     const [data, setData] = useState([]);
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setData(data))
     }, []);
+
+          
+    
+    
     return (
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-24 mb-12">
@@ -20,7 +27,7 @@ const Cards = () => {
 };
 
 Cards.propTypes = {
-
+    searchValue:PropTypes.string
 };
 
 export default Cards;
